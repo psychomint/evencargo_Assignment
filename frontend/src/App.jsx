79ebe29732c297a76672fd36d3293d1d4103b380
Brijesh Shukla <bkshukla229@gmail.com>
2025-10-05 16:39:26 +0530

    Frontend setup using react+parcel+tailwind
    Signed-off-by: Brijesh Shukla <bkshukla229@gmail.com>

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='border-2 border-amber-600 bg-amber-800 text-2xl text-black'>
+        React Setup with Tailwind
+        </div>
+  )
+}
+
+export default App
