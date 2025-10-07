26565ddca2ab0b6fc05bcd33545dc8a7471b8ceb
Brijesh Shukla <bkshukla229@gmail.com>
2025-10-07 10:01:24 +0530

    Make changes for deployment
    Signed-off-by: Brijesh Shukla <bkshukla229@gmail.com>

--- backend/DB/DB_CONNECTION.js
+++ backend/DB/DB_CONNECTION.js
@@ -1,18 +1,31 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'postgres' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false, 
-    },
-  },
-  logging: false,
+      rejectUnauthorized: false
+    }
+  }
 });
+
+
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   dialect: 'postgres' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false, 
+//     },
+//   },
+//   logging: false,
+// });
 
 (
     async () => {
