487ee0dfef247a3283820155351e5e38001c0c9b
Brijesh Shukla <bkshukla229@gmail.com>
2025-10-07 10:03:39 +0530

    Make changes for deployment
    Signed-off-by: Brijesh Shukla <bkshukla229@gmail.com>

--- backend/DB/DB_CONNECTION.js
+++ backend/DB/DB_CONNECTION.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const { Sequelize } = require("sequelize");
+// const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
